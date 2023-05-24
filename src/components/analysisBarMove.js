b95c867a59f81e30f171c98558405b9acbce5f61
David <davidmfstein@gmail.com>
2023-05-24 17:13:13 -0400

    style updates

--- src/components/analysisBarMove.js
+++ src/components/analysisBarMove.js
@@ -1,14 +1,21 @@
 import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { setCurrentMove } from '../reducers/gameSlice';
 
 const AnalysisBarMove = (props) => {
 
     const currentMove = useSelector((state) => state.game.currentMove);
     const white = currentMove % 2 === 0;
+    const dispatch = useDispatch()
+
+    const sendCurrentMove = (moveIndex) => {
+        dispatch(setCurrentMove(moveIndex));
+    }
 
     return  <tr key={props.index}>
                 <td>{props.index + 1}.</td>
-                <td className={currentMove === props.plies[0] && white  ? 'selected' : ''}>{props.movePair[0]}</td>
-                <td className={currentMove === props.plies[1] && !white ? 'selected' : ''}>{props.movePair[1]}</td>
+                <td onClick={() => sendCurrentMove(props.plies[0])} className={currentMove === props.plies[0] && white  ? 'selected' : ''}>{props.movePair[0]}</td>
+                <td onClick={() => sendCurrentMove(props.plies[1])} className={currentMove === props.plies[1] && !white ? 'selected' : ''}>{props.movePair[1]}</td>
             </tr>
 
 }
